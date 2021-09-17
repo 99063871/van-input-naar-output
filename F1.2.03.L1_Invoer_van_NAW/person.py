aaf695d943031e89e1f44fd8ae742e5b6d993987
99063871 <99063871@mydavinci.com>
2021-09-17 09:48:58 +0200

    invoer van NAW commiten

---
+++ F1.2.03.L1_Invoer_van_NAW/person.py
@@ -0,0 +1,15 @@
+print(' ----------------------------------------------------')
+
+Name = input('Wat is uw naam? ')
+print('|  Naam      : ' + Name)
+
+Adres = input('Wat is uw adres? ')
+print('|  Adres     : ' + Adres)
+
+Postcode = input('Wat is uw postcode? ')
+print('|  Postcode  : ' + Postcode)
+
+Woonplaats = input('Wat is uw woonplaats? ')
+print('|  Woonplaats  : ' + Woonplaats)
+
+print(' ----------------------------------------------------')
